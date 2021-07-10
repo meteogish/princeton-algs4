d62797a3fc95c62fff7c36a33673c846186383cc
meteogish <meteogish@gmail.com>
2021-07-10 19:02:57 +0200

    Week4 + Week5 (#2)
    * 🎉  Week3 started (not working)
    
    * Fixed pmd
    
    * Added getHashCode
    
    * Fixed hamming
    
    * Fixed manhattan for 0 position
    
    * Fixed for negative cases
    
    * Test
    
    * Neighbors impl
    
    * isGoal
    
    * First implementation of Solver
    
    * Solver
    
    * Introduced SearchNode
    
    * Changed is goal implementation
    
    * Board refactor to 1D
    
    * Full refactor. now I understood this sh*t
    
    * Refactor isGoal
    
    * Added Board.isSolvable impl
    
    * Board.twin impl
    
    * Added unsolvable impl
    
    * Assigment fixes 1
    
    * Stylecheck fixes
    
    * ⚡️ 100/100
    
    * Added readme
    
    * KdTree draw start
    
    * Draw impl. Insert fixes
    
    * Insert refactor, before I forgot everything
    
    * contains
    
    * Range/nearest impl started
    
    * Assesment's fixes.
    
    * 100% + cleanup

---
+++ src/main/java/Week5/KdTree.java
@@ -0,0 +1,218 @@
+import java.util.LinkedList;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class KdTree {
+    private static class Node {
+        public Point2D point;
+        public RectHV rect;
+        public Node left;
+        public Node right;
+    }
+
+    private Node root;
+
+    private int count;
+
+    public KdTree() // construct an empty set of points
+    {
+        root = null;
+        count = 0;
+    }
+
+    public boolean isEmpty() // is the set empty?
+    {
+        return count == 0;
+    }
+
+    public int size() // number of points in the set
+    {
+        return count;
+    }
+
+    public void insert(Point2D p) // add the point to the set (if it is not already in the set)
+    {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Node newNode = new Node();
+        newNode.point = p;
+        newNode.rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+        newNode.left = null;
+        newNode.right = null;
+
+        if (root == null) {
+            root = newNode;
+            ++count;
+        } else {
+            insert(root, newNode, true);
+        }
+    }
+
+    private void insert(Node currentNode, Node newNode, boolean isVerticalLevel) {
+        if (currentNode.point.equals(newNode.point)) {
+            return;
+        }
+
+        int compareResult = 0;
+        int rightRectangleType = 0;
+        int leftRectangleType = 0;
+        if (isVerticalLevel) {
+            compareResult = Double.compare(newNode.point.x(), currentNode.point.x());
+            // left or right rects only
+            rightRectangleType = 3;
+            leftRectangleType = 0;
+        } else {
+            compareResult = Double.compare(newNode.point.y(), currentNode.point.y());
+            // top or bottom rects only
+            rightRectangleType = 1;
+            leftRectangleType = 2;
+        }
+
+        if (compareResult < 0) {
+            if (currentNode.left == null) {
+                newNode.rect = getRectangle(currentNode.point, currentNode.rect, leftRectangleType);
+                currentNode.left = newNode;
+                ++count;
+            } else {
+                insert(currentNode.left, newNode, !isVerticalLevel);
+            }
+        } else {
+            if (currentNode.right == null) {
+                newNode.rect = getRectangle(currentNode.point, currentNode.rect, rightRectangleType);
+                currentNode.right = newNode;
+                ++count;
+            } else {
+                insert(currentNode.right, newNode, !isVerticalLevel);
+            }
+        }
+    }
+
+    private RectHV getRectangle(Point2D point, RectHV enclosingRect, int quarter) {
+        switch (quarter) {
+            // left
+            case 0:
+                return new RectHV(enclosingRect.xmin(), enclosingRect.ymin(), point.x(), enclosingRect.ymax());
+            // top
+            case 1:
+                return new RectHV(enclosingRect.xmin(), point.y(), enclosingRect.xmax(), enclosingRect.ymax());
+            // bottom
+            case 2:
+                return new RectHV(enclosingRect.xmin(), enclosingRect.ymin(), enclosingRect.xmax(), point.y());
+            // right
+            default:
+                return new RectHV(point.x(), enclosingRect.ymin(), enclosingRect.xmax(), enclosingRect.ymax());
+        }
+    }
+
+    public boolean contains(Point2D p) // does the set contain point p?
+    {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+        return containsRec(p, root);
+    }
+
+    private boolean containsRec(Point2D p, Node node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (!node.rect.contains(p)) {
+            return false;
+        }
+
+        return node.point.equals(p) || containsRec(p, node.left) || containsRec(p, node.right);
+    }
+
+    public void draw() // draw all points to standard draw
+    {
+        drawNode(root, true);
+    }
+
+    private void drawNode(Node node, boolean isVerticalLevel) {
+        if (node != null) {
+            StdDraw.setPenRadius(0.01);
+            node.point.draw();
+            StdDraw.setPenRadius();
+            if (isVerticalLevel) {
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.line(node.point.x(), node.rect.ymin(), node.point.x(), node.rect.ymax());
+            } else {
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.line(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.point.y());
+            }
+
+            StdDraw.setPenColor();
+            drawNode(node.left, !isVerticalLevel);
+            drawNode(node.right, !isVerticalLevel);
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) // all points that are inside the rectangle (or on the boundary)
+    {
+        if (rect == null) {
+            throw new IllegalArgumentException();
+        }
+        LinkedList<Point2D> found = new LinkedList<>();
+        return rangeRec(found, rect, root);
+    }
+
+    private LinkedList<Point2D> rangeRec(LinkedList<Point2D> agg, RectHV rect, Node node) {
+        if (node == null) {
+            return agg;
+        }
+
+        if (!node.rect.intersects(rect)) {
+            return agg;
+        }
+
+        if (rect.contains(node.point)) {
+            agg.add(node.point);
+        }
+
+        LinkedList<Point2D> leftResult = rangeRec(agg, rect, node.left);
+        return rangeRec(leftResult, rect, node.right);
+    }
+
+    public Point2D nearest(Point2D p) // a nearest neighbor in the set to point p; null if the set is empty
+    {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (root != null) {
+            return nearestRec(p, root.point, root, true);
+        }
+        return null;
+    }
+
+    private Point2D nearestRec(Point2D target, Point2D bestSoFar, Node node, boolean isVerticalLevel) {
+        if (node == null) {
+            return bestSoFar;
+        }
+
+        if (node.point.distanceSquaredTo(target) < bestSoFar.distanceSquaredTo(target)) {
+            bestSoFar = node.point;
+        }
+
+        boolean shouldGoLeft = isVerticalLevel ? node.point.x() > target.x() : node.point.y() > target.y();
+
+        if (shouldGoLeft) {
+            Point2D newBestSoFar = nearestRec(target, bestSoFar, node.left, !isVerticalLevel);
+            if (node.right != null && node.right.rect.distanceSquaredTo(target) < newBestSoFar.distanceSquaredTo(target)) {
+                return nearestRec(target, newBestSoFar, node.right, !isVerticalLevel);
+            }
+            return newBestSoFar;
+        } else {
+            Point2D newBestSoFar = nearestRec(target, bestSoFar, node.right, !isVerticalLevel);
+            if (node.left != null && node.left.rect.distanceSquaredTo(target) < newBestSoFar.distanceSquaredTo(target)) {
+                return nearestRec(target, newBestSoFar, node.left, !isVerticalLevel);
+            }
+            return newBestSoFar;
+        }
+    }
+}
---
+++ src/main/java/Week5/KdTreeVisualizer.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *  Compilation:  javac KdTreeVisualizer.java
+ *  Execution:    java KdTreeVisualizer
+ *  Dependencies: KdTree.java
+ *
+ *  Add the points that the user clicks in the standard draw window
+ *  to a kd-tree and draw the resulting kd-tree.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class KdTreeVisualizer {
+
+    public static void main(String[] args) {
+        //RectHV rect = new RectHV(0.3, 0.3, 0.8, 0.8);
+        RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+        StdDraw.enableDoubleBuffering();
+        rect.draw();
+        KdTree kdtree = new KdTree(); 
+        while (true) {
+            if (StdDraw.isMousePressed()) {
+                double x = StdDraw.mouseX();
+                double y = StdDraw.mouseY();
+                StdOut.printf("%8.6f %8.6f\n", x, y);
+                Point2D p = new Point2D(x, y);
+                if (rect.contains(p)) {
+                    //StdOut.printf("%8.6f %8.6f\n", x, y);
+                    StdOut.printf("Size: %d\n", kdtree.size());
+                    kdtree.insert(p);
+                    StdDraw.clear();
+                    kdtree.draw();
+                    rect.draw();
+                    StdDraw.show();
+                }
+            }
+            StdDraw.pause(20);
+        }
+
+    }
+}
